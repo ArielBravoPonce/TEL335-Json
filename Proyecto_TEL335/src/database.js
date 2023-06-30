cfb927044aa82d7e2e66e7970e210d89ff9e7e76
Ariel Bravo Ponce <ariel.bravo.p99@gmail.com>
2023-06-29 23:21:30 -0400

    Se sube proyecto final

--- Proyecto_TEL335/src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/test_tel335';
-
-mongoose.connect(uri)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
-
-module.exports = mongoose
--- Proyecto_TEL335/src/models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const TaskSchema = new Schema({
-    title: {type: String, require: true},
-    description: {type:String, requiere : true}
-});
-
-module.exports = mongoose.model('Task', TaskSchema);
